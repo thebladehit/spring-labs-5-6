e17f8b905964febba3ff051f3274ed1786c5fd85
thebladehit <boqdanyarmolka@gmail.com>
2024-10-06 21:08:37 +0300

    Initial setup

---
+++ src/main/java/spring/labs/dating/SpringLabs56Application.java
@@ -0,0 +1,13 @@
+package spring.labs.dating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabs56Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLabs56Application.class, args);
+    }
+
+}
